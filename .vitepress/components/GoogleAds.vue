be78a5a86e8c1000b99bfc0b5375d9e244d8d07e
xiashihua <xiashihua@kingsoft.com>
2024-03-18 19:41:21 +0800

    add google ad-test

--- .vitepress/components/GoogleAds.vue
+++ .vitepress/components/GoogleAds.vue
@@ -1,8 +1,14 @@
-<script setup lang="ts">
-
+<script setup lang='ts'>
+  const adsbygoogle = window.adsbygoogle || [];
+  adsbygoogle.push({});
 </script>
 <template>
-  ad-test
+  <ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-8990964151180359"
+     data-ad-slot="7980586236"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
 </template>
 <style scoped>
 
