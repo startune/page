564b9334a24a317b982f668a0f7f533b6075a1bf
xiashihua <xiashihua@kingsoft.com>
2024-03-18 19:48:55 +0800

    add google ad-test

--- .vitepress/components/GoogleAds.vue
+++ .vitepress/components/GoogleAds.vue
@@ -7,13 +7,17 @@ onMounted(() => {
 })
 </script>
 <template>
-  <ins class="adsbygoogle"
+  <div class="google-ads">
+    <ins class="adsbygoogle"
      style="display:block"
      data-ad-client="ca-pub-8990964151180359"
      data-ad-slot="7980586236"
      data-ad-format="auto"
      data-full-width-responsive="true"></ins>
+  </div>
 </template>
 <style scoped>
-
+.google-ads {
+  height: 100px; 
+}
 </style>
